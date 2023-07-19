7c74e821ae28206f1f207fd1bf60ad676742bd5f
Mekan Hojayev <71170609+Mekan-Hojayev@users.noreply.github.com>
2023-07-19 13:01:16 +0300

    text_similarity.py

--- text_similarity.py
+++ text_similarity.py
@@ -6,6 +6,6 @@ nlp = spacy.load("ru_core_news_sm")
 
 def text_similarity(text1: str, text2: str):
     text1_nlp = nlp(text1)
-    text2_nlp = nlp(text2) 
+    text2_nlp = nlp(text2)
 
-    print('Similarity score of documents is:', text1_nlp.similarity(text2_nlp))
+    return text1_nlp.similarity(text2_nlp)
